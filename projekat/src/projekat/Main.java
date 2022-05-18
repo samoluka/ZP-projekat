5682105adf2e919b06155f4561a143c94b279eb8
Luka <lukastojanovic314@gmail.com>
2022-05-18 18:47:18 +0200

    Krenuo gui

--- projekat/src/projekat/Main.java
+++ projekat/src/projekat/Main.java
@@ -1,7 +1,33 @@
 package projekat;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("CAO FIKUSE");
+		JFrame frame = new JFrame("My First GUI");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 300);
+		JButton button = new JButton("STISNI ME FIKUSE");
+		button.setBackground(Color.red);
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JButton b = (JButton) e.getSource();
+				if (b.getText().equals("STISNI ME FIKUSE")) {
+					b.setText("DOBRO GA STISCES");
+					b.setBackground(Color.green);
+				} else {
+					b.setText("STISNI ME FIKUSE");
+					b.setBackground(Color.red);
+				}
+			}
+		});
+		frame.getContentPane().add(button); // Adds Button to content pane of frame
+		frame.setVisible(true);
 	}
 }
