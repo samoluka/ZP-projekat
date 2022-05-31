93efdee2b9e50e0b15b1f19dff5e54e789bb6b42
Filip Kojić <filipkojic48@gmail.com>
2022-05-31 03:44:52 +0200

    Dodate metode za brisanje javnih i privatnih kljuceva uz komentar za koriscenje

--- projekat/src/projekat/Keys.java
+++ projekat/src/projekat/Keys.java
@@ -17,6 +17,7 @@ import org.bouncycastle.openpgp.PGPSecretKey;
 import org.bouncycastle.openpgp.PGPSecretKeyRing;
 import org.bouncycastle.openpgp.PGPSecretKeyRingCollection;
 import org.bouncycastle.openpgp.operator.bc.BcKeyFingerprintCalculator;
+import org.bouncycastle.openpgp.operator.jcajce.JcePBESecretKeyDecryptorBuilder;
 
 public class Keys {
 
@@ -121,7 +122,7 @@ public class Keys {
 		publicKeyRing.encode(aos);
 		aos.close();
 	}
-	
+
 	public void publicKeyImport(String importPath) throws IOException, PGPException {
 		PGPPublicKeyRingCollection currentPKRC = UserProvider.getInstance().getCurrentUser()
 				.getPublicKeyRingCollection();
@@ -147,7 +148,7 @@ public class Keys {
 		privateKeyRing.encode(aos);
 		aos.close();
 	}
-	
+
 	public void privateKeyImport(String importPath) throws IOException, PGPException {
 		PGPSecretKeyRingCollection currentSKRC = UserProvider.getInstance().getCurrentUser()
 				.getSecretKeyRingCollection();
@@ -166,4 +167,77 @@ public class Keys {
 		}
 	}
 
+	// metoda za brisanje kljuceva
+	// oni su radili tako da javni kljuc moze da se obrise uvijek, a da kad se brise
+	// privatni automatski se obrise i njegov odgovarajuci javni
+	// prvi dio mi ima smisla, jer u sustini svoj javni kljuc uvijek mozes da
+	// obrises jer ces i dalje moci da nesto sto je njim sifrovano
+	// od strane drugih korisnika desifrujes sa odgovarajucim privatnim kljucem, a i
+	// tudji javni svakako mozes uvijek da obrises
+	// samo sto neces vise moci njim da sifrujes
+	// nisam siguran da li kod brisanja privatnog kljuca da automatski obrisemo i
+	// javni zato sto je razlika u odnosu na njihov projekat
+	// to sto se kod nas i ukoliko korisnik obrise svoj privatni i dalje ostali
+	// korisnici mogu imati odgovarajuci javni kljuc tog privatnog
+	// kljuca i mogu da kriptuju poruku sa njime (u sustini mi bismo onda morali da
+	// iteriramo kroz prstenove svih korisnika i da brisemo
+	// taj javni kljuc, ali mislim da to nije u okviru ovog projekta vec u okviru
+	// povlacenja janih kljuceva - poslednji slajd PGP prezentacije)
+	// znaci kod njih je to ok jer nakon tog brisanja jednog privatnog vise ne moze
+	// niko da sifruje sa odgovarajucim javnim
+
+	// e sad, posto se u postavci naglasava generisanje i brisanje PARA kljuceva
+	// mozda je ipak bolje da je uradjeno kao i kod anje, s tim sto cemo pri
+	// eventualnom testiranju izbjegavati da sifrujemo
+	// necijim javnim ako znamo da je njegov odgovarajuci privatni kljuc obrisan
+
+	// znaci na kraju
+	// pri brisanju javnog kljuca ostavicemo odgovarajuci privatni, dok cemo pri
+	// brisanju privatnog obrisati i njegov odgovarajuci javni
+
+	// u gui dijelu za pregled kljuceva moze se dodati dugme obrisi
+	// pri kliku na dugme obrisi kod javnog kljuca, metodu deletePublicKey zvati sa
+	// id kljuca i objektom trenutnog usera
+	// a pri kliku na dugme obrisi kod privatnog kljuca, prvo pitaj za sifru od tog
+	// kljuca i zatim pozovi metodi
+	// deletePrivateKey sa id kljuca, unesenom sifrom i objektom trenutnog usera
+	// ukoliko se unese pogresna sifra baci se PGPException i vraca se false
+	// brisanje javnog kljuca se uvijek uspijesno izvrsi
+
+	public boolean deletePrivateKey(long keyID, String password, User user) throws IOException {
+		PGPSecretKeyRingCollection SCRC = user.getSecretKeyRingCollection();
+		PGPSecretKeyRing secretRing = findPrivateRing(keyID, user);
+
+		File SCRD = user.getSecretKeyRingDirectory();
+
+		try {
+			secretRing.getSecretKey().extractPrivateKey(
+					new JcePBESecretKeyDecryptorBuilder().setProvider("BC").build(password.toCharArray()));
+		} catch (PGPException e) {
+			// e.printStackTrace();
+			return false;
+		}
+		SCRC = PGPSecretKeyRingCollection.removeSecretKeyRing(SCRC, secretRing);
+		ArmoredOutputStream aos = new ArmoredOutputStream(new FileOutputStream(SCRD));
+		SCRC.encode(aos);
+		aos.close();
+
+		deletePublicKey(keyID, user);
+
+		return true;
+	}
+
+	public void deletePublicKey(long keyID, User user) throws IOException {
+		PGPPublicKeyRingCollection PCRC = user.getPublicKeyRingCollection();
+		File PCRD = user.getPublicKeyRingDirectory();
+		PGPPublicKeyRing publicRing = findPublicRing(keyID, user);
+
+		if (publicRing != null) {
+			PCRC = PGPPublicKeyRingCollection.removePublicKeyRing(PCRC, publicRing);
+			ArmoredOutputStream aos = new ArmoredOutputStream(new FileOutputStream(PCRD));
+			PCRC.encode(aos);
+			aos.close();
+		}
+		// initialize(path);  zasad nek stoji, posle brisemo ako ne valja
+	}
 }
